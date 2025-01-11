f0f49986214fb323c7b44572c98f4edadea88802
Kenny <kennethmadondo01gmail.com>
2025-01-11 19:35:41 +0200

    update about us

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,12 +10,12 @@ const About = () => {
   return (
       <section className="about-page ">
          <h1>Welcome to VarsitySteps</h1>
-         <p>At VarsitySteps we are dedicated to empowering students in Zimbabwe by providing the resources and support they need to succeed in their academic and career journeys. Our platform is designed to address common challenges faced by students of all levels, from primary up to varsity,by offering a comprehensive suite of tools and information to help them navigate their educational and professional paths with confidence. The initial plan is to provide a tutoring platofrm to help both student and tutor get the best result from VarsitySteps.</p> <br />
-         <p>From career guidance to a network of tutors and other information, VarsitySteps is your go-to destinaton for everything you need to make informed decisions and achieve your goals. Join us as we strive to create a brighter future for students across Zimbabwe, one step at a time.</p><br />
+         <p>At VarsitySteps we are dedicated to empowering students in Zimbabwe by providing the resources and support they need to succeed in their academic and career journeys. Currently VasritySteps is designed to mainly to connect students and tutors nationwide to help both student and tutor get the best result from VarsitySteps but why stop there.</p> <br />
+         <p>We plan on adding more features  that will benefit the education community in Zimbabwe and expnading VarsitySteps into an all inclusive platfomrm for students accross the country.</p><br />
          <h3>Our Story</h3>
-         <p>VarsitySteps was developed by students, who had a vision to bridge the gap between students and the resources they need to excel. As students passionate about education and student success, we recognized the challenges faced by students in Zimbabwe- challenges that often hinder their academic and career ambitions.</p><br/>
+         <p>VarsitySteps developed by a student, who had a vision to bridge the gap between students and the resources they need to excel. As students passionate about education and student success, we recognized the challenges faced by students in Zimbabwe- challenges that often hinder their academic and career ambitions, and that is our driving force, to make education enjoyable and not just bearable</p><br/>
          <p>With this in mind, we set out to create a platform that would address these issues head-on. Drawing from my own experiences and insights, We developed VarsitySteps to be more than just a tool- it's a community dedicated to empowering students. Through this platform, we aim to provide not only the information and opportunities students need but also a supportive network to guide them through their educational journeys</p><br />
-         <p>VarsitySteps is the culminationof a commitment to making a meaningful difference in students' lives. Every feature and resources on the platform is designed with their needs in mind, reflecting our dedication to their success growth.</p><br />
+         <p>VarsitySteps is the culmination of a commitment to making a meaningful difference in students' lives including alumni. Every feature and resources we plan to introduce on the platform will be designed with their needs in mind, reflecting our dedication to their success growth.</p><br />
 
          <h3>How to support VarsitySteps</h3>
          <h4>Financial Support</h4>
@@ -31,7 +31,7 @@ const About = () => {
          <p>Support doesn’t always have to be monetary or technical. Share VarsitySteps with your friends, family, and networks. The larger our community grows, the more opportunities we can provide to students everywhere.</p><br />
 
          <h4>Engage with the Platform</h4>
-         <p>Use VarsitySteps regularly! Whether it’s becoming a tutor, contributing to the creator section, or exploring the various resources, your engagement motivates us to keep innovating and serving our community.</p><br />
+         <p>Use VarsitySteps regularly! Whether it’s becoming a tutor,  or being engaged with the platform motivates us to keep innovating and serving our community.</p><br />
          <p>Together, we can make VarsitySteps a leading hub for student empowerment. Every little bit counts, and your support, in any form, brings us closer to achieving this vision!</p>
          <h3>The Team</h3>
          <div className="teamgrid">
@@ -45,7 +45,7 @@ const About = () => {
               <Link href='https://wa.me/+263789644097' target='_blank'><FontAwesomeIcon icon={faWhatsapp}/></Link>
               <Link href='https://www.instagram.com/kennethmadondo3' target='_blank'><FontAwesomeIcon icon={faInstagram}/></Link>
               <Link href='https://facebook.com/Kennethmadondo2' target='_blank'><FontAwesomeIcon icon={faFacebook}/></Link>
-              <Link href='https://wwww.linkedin.com/in/kenneth-madondo-099b26287' target='_blank'><FontAwesomeIcon icon={faLinkedin}/></Link>
+              <Link href='https://www.linkedin.com/in/kenneth-madondo-099b26287' target='_blank'><FontAwesomeIcon icon={faLinkedin}/></Link>
             </div>
           </div>
          </div>
