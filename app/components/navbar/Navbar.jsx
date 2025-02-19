3cd1a141cdb5ef1e21f1eb7fe3138dbc0a7d97bd
Kenny <kennethmadondo01gmail.com>
2025-02-19 07:36:13 +0200

    will sort out notification icon later

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -96,9 +96,9 @@ const Navbar = () => {
         <button className="showdash-widescreen" onClick={()=>setShowDash(!showDash)} >
         {userData.role_name.includes("student")?<Link href="/dashboard"><FontAwesomeIcon icon={faUser}/></Link>:<Link href="/tutordashboard"><FontAwesomeIcon icon={faUser}/></Link>}
       </button>
-      <button className="showdash-widescreen" onClick={()=>setShowNotifications(true)} >
+      {/* <button className="showdash-widescreen" onClick={()=>setShowNotifications(true)} >
         <FontAwesomeIcon icon={faBell}/>
-      </button>
+      </button> */}
       {isAuthenticated == true && userData && <button onClick={()=>setShowNotifications(true)} className='notification-icon'><FontAwesomeIcon icon={faBell}/><span>{notificationCount>0?notificationCount:''}</span></button>}
 
         </div> : <div className="login-btn">
