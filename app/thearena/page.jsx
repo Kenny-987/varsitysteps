e22f7817b38bb4a35d939a3ac6afc62bf7544d8f
Kenny <kennethmadondo01gmail.com>
2025-04-14 09:39:59 +0200

    arena maintenance notice

--- app/thearena/page.jsx
+++ app/thearena/page.jsx
@@ -27,12 +27,13 @@ const Arena = () => {
     </Head>
    
     <section className="arena">
-        {/* <div className="coming-soon">
-            <h1>Coming soon</h1>
-            <p>quizzes are being added</p>
-        </div> */}
+        <div className="coming-soon">
+            <h1>Under Maintenance</h1>
+            <p>Arena section will be live soon</p>
+            <Image src='/images/construction.svg' width={100} height={100}/>
+        </div>
         
-        <div className="heading">
+        {/* <div className="heading">
             <h3>Welcome to the Arena</h3>
             <p>Complete challenges and climb up the Leaderboard</p>
         </div>
@@ -55,7 +56,7 @@ const Arena = () => {
                 </div>}
                 
             {content === 'leaderboard' && <LeaderBoard/>}
-        </div>
+        </div> */}
     </section>
     </>
   )
